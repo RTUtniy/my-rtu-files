4c7037e876ee7790acaf805bc517047863e68758
RTUtniy <iaroslav.viazmitin@edu.rtu.lv>
2025-09-22 14:00:04 +0300

    2 labdarbs

--- III semestris/Skaitliskās metodes/080925_Prakse.m
+++ III semestris/Skaitliskās metodes/080925_Prakse.m
@@ -1,9 +1,9 @@
-%% Line�ru vien�dojumu sist�mas. Tie��s metodes 
+%% Lineāru vienādojumu sistēmas. Tie��s metodes 
 % 7.-39.lpp.
 
-%% Visu uzdevumu numer�cija sakr�t ar uzdevumu numer�ciju
-% m�c�bu gr�mat� V.Koli�kina, A.Koli�kins. 
-% Skaitlisk�s metodes ar Matlab. RTU izdevniec�ba,2023.
+%% Visu uzdevumu numerācija sakrīt ar uzdevumu numerāciju
+% mācību grāmatā V.Koliškina, A.Koliškins. 
+% Skaitliskās metodes ar Matlab. RTU izdevniecība, 2023.
 
 %% Gausa metode: 7.-16.lpp.
 
@@ -14,14 +14,14 @@ B = [-3;5;3];
 [row,col] = size(A)
 Aaug = [A B];          % divu matricu apvienojums
 A_rank = rank(A)       % matricas rangs
-Aaug_rank = rank(Aaug) % papla�in�t�s matricas rangs
+Aaug_rank = rank(Aaug) % paplašinātās matricas rangs
 sol = rref(Aaug)       % Ctrl+Enter
 
-% turpin�jums
+% turpinājums
 X_name = ['x1';'x2';'x3'];
 X_value = sol(:,col+1);
 disp('Atbilde:')
-disp(' LVS ir sader�ga un noteikta,tai ir viens vien�gs atrisin�jums')
+disp(' LVS ir saderīga un noteikta,tai ir viens vienīgs atrisinājums')
 solution = table(X_name,X_value)
 
 %% 1.2. uzdevums
@@ -36,14 +36,14 @@ A_rank = rank(A)
 Aaug_rank = rank(Aaug) 
 sol = sym(rref(Aaug))  % Ctrl+Enter
 
-% turpin�jums 
+% turpinājums 
 syms z, X_gen = sol(:,4)-sol(:,3).*z   % Ctrl+Enter
 
-% turpin�jums
+% turpinājums
 disp('Atbilde:')
-disp(' LVS ir sader�ga un nenoteikta (bezgal�gi daudz atrisin�jumu)')
+disp(' LVS ir saderīga un nenoteikta (bezgalīgi daudz atrisinājumu)')
 fprintf('x = %s, y = %s; \n', X_gen(1:2))
-fprintf('z - jebkur� re�ls skaitlis\n')
+fprintf('z - jebkurš reāls skaitlis\n')
 
 %% 1.3. uzdevums
 clc, clearvars, format compact 
@@ -57,17 +57,17 @@ A_rank = rank(A)
 Aaug_rank = rank(Aaug) 
 sol = sym(rref(Aaug))  % Ctrl+Enter
 
-% turpin�jums 
+% turpinājums 
 syms x4
 X_gen = sol(:,5)-sol(:,4).*x4   % Ctrl+Enter
 X_part = subs(X_gen, x4, -2)
 x1_ver = X_part(1)
 
-% turpin�jums
+% turpinājums
 disp('Atbilde:')
-disp(' LVS ir sader�ga un nenoteikta (bezgal�gi daudz atrisin�jumu)')
+disp(' LVS ir saderīga un nenoteikta (bezgalīgi daudz atrisinājumu)')
 fprintf('x1 = %s, x2 = %s, x3=%s; \n', X_gen(1:3))
-fprintf('x4 - jebkur� re�ls skaitlis\n\n')
+fprintf('x4 - jebkurš reāls skaitlis\n\n')
 fprintf('x1 = %s\n', x1_ver)
 
 %% 1.4. uzdevums
@@ -83,17 +83,17 @@ A_rank = rank(A)
 Aaug_rank = rank(Aaug) 
 sol = sym(rref(Aaug))  % Ctrl+Enter
 
-% turpin�jums 
+% turpinājums 
 syms x3 x4
 X_gen = sol(:,5)-sol(:,4).*x4-sol(:,3).*x3   % Ctrl+Enter
 X_part = subs(subs(X_gen, x3, -1), x4, 3)
 x2_ver = X_part(2)
 
-% turpin�jums
+% turpinājums
 disp('Atbilde:')
-disp(' LVS ir sader�ga un nenoteikta (bezgal�gi daudz atrisin�jumu)')
+disp(' LVS ir saderīga un nenoteikta (bezgalīgi daudz atrisinājumu)')
 fprintf('x1 = %s, x2 = %s; \n', X_gen(1:2))
-fprintf('x3 un x4 - jebkuri re�li skait�i\n\n')
+fprintf('x3 un x4 - jebkuri reāli skaitļi\n\n')
 fprintf('x2 = %s\n', x2_ver)
 
 %% 1.9. uzd
@@ -110,7 +110,7 @@ Aaug_rank = rank(Aaug)
 sol = sym(rref(Aaug))  % Ctrl+Enter
 
 if Aaug_rank ~= A_rank
-    fprintf('Atbilde:\n LVS ir nesader�ga (nav atrsin�juma)\n')
+    fprintf('Atbilde:\n LVS ir nesaderīga (nav atrsinājuma)\n')
 end
 
 %% 1.10. uzd
@@ -124,14 +124,14 @@ A_rank = rank(A)
 Aaug_rank = rank(Aaug) 
 sol = sym(rref(Aaug))  % Ctrl+Enter
 
-% turpin�jums 
+% turpinājums 
 syms x4 x5
 X_gen = sol(:,6)-sol(:,5).*x5-sol(:,4).*x4   % Ctrl+Enter
 X_part = subs(subs(X_gen, x4, -2), x5, -1)
 x3_ver = X_part(3)
 
 disp('Atbilde:')
-disp(' LVS ir sader�ga un nenoteikta (bezgal�gi daudz atrisin�jumu)')
+disp(' LVS ir saderīga un nenoteikta (bezgalīgi daudz atrisinājumu)')
 fprintf(' x1 = %s, x2 = %s, x3 = %s; \n', X_gen(1:3))
-fprintf(' kur x4 un x5 - jebkuri re�li skait�i\n\n')
+fprintf(' kur x4 un x5 - jebkuri reāli skaitļi\n\n')
 fprintf('x3 = %s\n', x3_ver)
---
+++ III semestris/Skaitliskās metodes/m220925_Prakse.m
@@ -0,0 +1,210 @@
+%% Levickas ievads
+
+% Jakoba metode - svarīga štelle
+% Kontroldarbā būs 3 Gausa, 2 Jakobi uzdevumi
+% ChatGPT izmantošana ir ok, izņemot kontroldarbu laikā
+
+%% Uzdevumi patstâvîgai risinâðanai
+% 2.1. - 2.5.uzdevumi ( 64. - 68.lpp.)
+
+%% 2.3. uzdevums
+clc, clearvars, format long
+A = [13,10,13;10,8,10;13,10,14];  B = [2;22;3];
+if det(A) == 0
+    disp('Matrica A ir singulārā ')
+    disp('Atbilde: Jakobi metodi nedrīkst izmantot'), return
+end
+disp('Matrica A ir nesingulārā')
+fun_prob3(A)  % pârbaude: Jakobi metode konverģē
+
+x_app = zeros(3,1);  % sākuma tuvinājums 
+iter = 0;            % iterāciju skaits
+itermax = 25;        % maksimālais iterāciju skaits
+epsi = 0.001;        % epsi=10^(-3) (aprēķinu precizitāte)
+errnorm = 1;         % kļūdas norma
+prnorm = zeros(1,2); % datu masīvs rezultātu saglabāšanai
+k = 1;
+
+while errnorm > epsi && iter < itermax 
+    k = k+1;  iter  = iter+1;
+    for rowIndex = 1:3
+      res_sum = 0;
+      for columnIndex = 1:3
+         if columnIndex~=rowIndex
+           res_sum =res_sum+x_app(columnIndex,k-1)*A(rowIndex,columnIndex);
+         end
+      end
+      x_app(rowIndex,k) = (B(rowIndex,1)-res_sum)/A(rowIndex,rowIndex);
+    end
+    errnorm  =norm(x_app(:,k)-x_app(:,k-1));
+    prnorm(iter,:) =[iter,errnorm];
+end
+
+table(prnorm, 'VariableNames', "Iter.numurs      Kļūda")
+x_approx = x_app(:,k)
+x_sol = linsolve(A,B)
+
+[row,col] = size(prnorm);
+disp('Atbilde:')
+fprintf(' iterāciju skaits = %.0f\n',prnorm(row,1)) 
+fprintf(' kļūda = %.6f\n' ,prnorm(row,2))
+fprintf(' x_tuvinājumi : { %.4f , %.4f, %.4f }\n',x_approx(:)')
+disp(" Jakobi metode diverģē")
+
+%% 2.4. uzdevums
+clc, clearvars, format long
+A = [2,14,10;
+     14,2,8;
+     10,8,2];
+B = [8;
+     6;
+     2];
+
+if det(A) == 0
+    disp('Matrica A ir singulārā ')
+    disp('Atbilde: Jakobi metodi nedrīkst izmantot'), return
+end
+disp('Matrica A ir nesingulārā')
+fun_prob3(A)  % pârbaude: Jakobi metode konverģē
+
+x_app = zeros(3,1);  % sākuma tuvinājums 
+iter = 0;            % iterāciju skaits
+itermax = 13;        % maksimālais iterāciju skaits
+epsi = 0.001;        % epsi=10^(-3) (aprēķinu precizitāte)
+errnorm = 1;         % kļūdas norma
+prnorm = zeros(1,2); % datu masīvs rezultātu saglabāšanai
+k = 1;
+
+% uzdevuma jautājumi
+normepsilon_11 = 0;
+normepsilon_10 = 0;
+normepsilon_13 = 0;
+
+while errnorm > epsi && iter < itermax 
+    k = k+1; iter = iter+1;
+    for rowIndex = 1:3
+      res_sum = 0;
+      for columnIndex = 1:3 % sigma
+         if columnIndex~=rowIndex
+           res_sum = res_sum + x_app(columnIndex, k-1) * A(rowIndex, columnIndex);
+         end
+      end
+      x_app(rowIndex,k) = (B(rowIndex,1) - res_sum)/A(rowIndex,rowIndex); % x_i^(n)
+    end
+    errnorm = norm(x_app(:,k) - x_app(:,k-1));
+    if k == 11
+        normepsilon_11 = errnorm
+    end
+    if k == 10
+        normepsilon_10 = errnorm
+    end
+    if k == 13
+        normepsilon_13 = errnorm
+    end
+    prnorm(iter,:) = [iter,errnorm];
+end
+
+disp(table(prnorm, 'VariableNames', "Iter.numurs      Kļūda"))
+
+x_approx = x_app(:,k)
+x_sol = linsolve(A, B)
+fprintf("norm(x_sol-x^10) = %.4f\n", norm(x_sol - x_app(:, 10)))
+
+[row,col] = size(prnorm);
+disp(prnorm)
+disp('Atbilde:')
+fprintf(' iterāciju skaits = %.0f --> kļūda = %.6f\n',prnorm(row,1),prnorm(row,2)) 
+fprintf(' iterāciju skaits = %.0f --> kļūda = %.6f\n',prnorm(row-2,1),prnorm(row-2,2))
+fprintf(' x_tuvinājumi : { %.4f , %.4f, %.4f }\n',x_approx(:)')
+%% 2.5. uzdevums
+clc, clearvars, format long
+A= [14,3,8;
+    3,12,7;
+    8,7,17];
+B= [5;
+    2;
+    6];
+
+if det(A) == 0
+    disp('Matrica A ir singulārā ')
+    disp('Atbilde: Jakobi metodi nedrīkst izmantot'), return
+end
+disp('Matrica A ir nesingulārā')
+fun_prob3(A)  % pârbaude: Jakobi metode konverģē
+
+x_app = zeros(3,1);  % sākuma tuvinājums 
+iter = 0;            % iterāciju skaits
+itermax = 9;        % maksimālais iterāciju skaits
+epsi = 0.001;        % epsi=10^(-3) (aprēķinu precizitāte)
+errnorm = 1;         % kļūdas norma
+prnorm = zeros(1,2); % datu masīvs rezultātu saglabāšanai
+k = 1;
+
+% uzdevuma jautājumi
+epsilon_6 = 0;
+epsilon_5 = 0;
+epsilon_9 = 0;
+
+
+% turpinâjums
+while errnorm > epsi && iter < itermax 
+    k = k+1;  iter  = iter+1;
+    for rowIndex = 1:3
+      res_sum = 0;
+      for columnIndex = 1:3
+         if columnIndex~=rowIndex
+           res_sum =res_sum+x_app(columnIndex,k-1)*A(rowIndex,columnIndex);
+         end
+      end
+      x_app(rowIndex,k) =(B(rowIndex,1)-res_sum)/A(rowIndex,rowIndex);
+    end
+    errnorm  =norm(x_app(:,k)-x_app(:,k-1));
+    prnorm(iter,:) =[iter,errnorm];
+end
+%disp('Iter.numurs  Kļūda')
+
+table(prnorm, 'VariableNames', ["Iter.numurs      Kļūda"])         % Ctrl+Enter
+x_approx = x_app(:,k)
+x_sol = linsolve(A,B) % Ctrl+Enter
+
+[row,col] = size(prnorm);
+disp('Atbilde:')
+fprintf(' iterāciju skaits = %.0f\n',prnorm(row,1)) 
+fprintf(' kļūda = %.6f\n' ,prnorm(row,2))
+fprintf(' x_tuvinājumi : { %.4f , %.4f, %.4f }\n',x_approx(:)')
+
+%% ârçjas funkcijas %%  ârçjas funkcijas  %%   ârçjas funkcijas %%%%%%%%%%
+% pamatprogrammas beigas
+
+
+% ârçja funkcija ( 1.16.piemçrs ). Faktorizâcijas  metode
+function fun_prob16(a_el,b_el,c_el)
+   n = length(a_el);
+   for i = 1:n
+      if abs(b_el(i)) < ( abs(a_el(i))+abs(c_el(i)) )
+         disp(' Faktorizâcijas metode nav stabila'), return
+      end
+    end
+    disp(' Faktorizâcijas metode ir stabila')  
+end
+
+
+% ârçja funkcija ( 2.3.piemçrs ). Jakobi metode
+% pârbaude: vai Jakobi metode konverìç?
+function fun_prob3(A_mat)
+ [row,col] = size(A_mat);
+ for i = 1:row
+    sum =0;
+    for j = 1:col
+        if i~=j
+            sum = sum+abs(A_mat(i,j));
+        end
+    end
+    if abs(A_mat(i,i)) <= sum
+      disp(' Neizpildās konverģences pietiekamais nosacījums')
+      fprintf(' rindas numurs %.0f: --> %.0f < %.0f \n', i,A_mat(i,i),sum ) 
+      return
+    end
+ end
+ disp(' Izpildâs konver.pietiekamais nosacîjums - Jakobi metode konverìç')
+end
